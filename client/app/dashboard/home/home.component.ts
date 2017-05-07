e149a1b3ec40d073ba3a326ac2d3001dc2bddfba
Gabriel Safado <gabriel.jacquier@tegra.me>
2017-05-06 23:04:06 -0300

    Adiciona dados cadastrais CNPJ

--- client/app/dashboard/home/home.component.ts
+++ client/app/dashboard/home/home.component.ts
@@ -22,9 +22,27 @@ export class HomeComponent implements OnInit {
            "cnpj": "04.325.815/0001-00",
            "razaoSocial": "DISPARE TRANSP ROD COM ATACADISTA HORTIFRUTIGRANJEIRO LTDA ME",
            "tipoSociedade": "SOCIEDADE EMPRESARIA LIMITADA",
-           "antecessora": "DISPARE TRANSPORTES LTDA ME",
+           "antecessoraNome":"DISPARE TRANSPORTES LTDA ME", 
+           "antecessoraAte": "07/07/2009",
            "registro": "4.155.487",
-           "inscricaoEstadual": "702129520067"
+           "dataRegistro": "07/06/2009",
+           "nire": "31.206.172.236",
+           "inscricaoEstadual": "702129520067",
+           "endereco":"ROD BR 050 50 KM 76 BOX 28 PAVILHO GP 1 A",
+           "bairro": "SEGISMUNDO",
+           "cidade": "UBERLANDIA",
+           "telefone": "(034) 4111-2022",
+           "cep": "38408-369",
+           "fax": "(034)",
+           "fundacao": "19/01/2002",
+           "filiais": "UBERLANDIA",
+           "ramo": "COM ATACADISTAS DE FRUTAS",
+           "quantidadeFiliais": "1",
+           "codigoAtividadeSerasa": "C-04.03.00",
+           "cnae": "46.338-01",
+           "opcaoTributaria": "SIMPLES NACIONAL",
+           "situacaoCnpj": "ATIVA"
+
         },
         opened: false
       }
@@ -34,6 +52,6 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     initDemo();
 
-    introJs().start();
+    // introJs().start();
   }
 }
